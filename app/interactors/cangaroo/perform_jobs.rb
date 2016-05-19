a1c0c4558ee027affc10cee59c852bd5d5894b54
Michael Bianco <iloveitaly@gmail.com>
2016-05-19 17:16:15 -0400

    Log payloads that are skipped becuse no jobs were run

--- app/interactors/cangaroo/perform_jobs.rb
+++ app/interactors/cangaroo/perform_jobs.rb
@@ -1,5 +1,6 @@
 module Cangaroo
   class PerformJobs
+    include Cangaroo::Log
     include Interactor
 
     def call
@@ -15,7 +16,18 @@ module Cangaroo
     end
 
     def enqueue_jobs(type, payload)
-      initialize_jobs(type, payload).select(&:perform?).each(&:enqueue)
+      initialize_jobs(type, payload).each do |job|
+        if job.perform?
+          # TODO log simplified info about payload info is being queued
+          job.enqueue
+        else
+          # TODO persist translation for audit trail?
+
+          log.info 'skipping payload, no jobs',
+            payload: payload,
+            payload_type: type
+        end
+      end
     end
 
     def initialize_jobs(type, payload)
