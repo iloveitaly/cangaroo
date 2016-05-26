afd68ba0cd4f1eec3f91c5d2ee14bd8a7c55ab1b
Michael Bianco <iloveitaly@gmail.com>
2016-05-25 21:11:22 -0400

    Generate unique random keys and tokens in connection factory

--- spec/factories/cangaroo_connections.rb
+++ spec/factories/cangaroo_connections.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     name :store
     url 'www.store.com'
     parameters { { first: 'first', second: 'second' } }
-    key '1e4e888ac66f8dd41e00c5a7ac36a32a9950d271'
-    token '8d49cddb4291562808bfca1bee8a9f7cf947a987'
+    key { SecureRandom.hex(13) }
+    token { SecureRandom.hex(13) }
   end
 end
